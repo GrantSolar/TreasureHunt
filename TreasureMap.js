c64a7033e70e37f4b54769d6d5a2d4b4617103f3
Oliver Davey <o.p.davey@gmail.com>
2015-11-12 21:35:32 +0000

    ensured 1 landmark max of each type

--- TreasureMap.js
+++ TreasureMap.js
@@ -5,13 +5,14 @@ emojione.imageType = 'png';
 emojione.imagePathPNG = './emojione-master/assets/png/';
 emojione.imagePathSVG = './emojione-master/assets/svg/';
 
-var items = [":house:",":church:",":school:",":hospital:",":bank:",":hotel:",":office:",":classical_building:",":stadium:",":department_store:",":european_post_office:",":airplane:",":steam_locomotive:"];
 
 function generate(size){
 	console.log("ready");
 	var tableSize = size + 1;
 	console.log("Size: "+size);
 	console.log("Table size: "+tableSize);
+	var items = [":house:",":church:",":school:",":hospital:",":bank:",":hotel:",":office:",":classical_building:",":stadium:",":department_store:",":european_post_office:",":airplane:",":steam_locomotive:"];
+	console.log(items);
 	var grid = "";
 	grid += "<tr><th></th>";
 	for(var i = 1; i<tableSize; i++)
@@ -25,14 +26,22 @@ function generate(size){
 		for(var j = 1; j<tableSize; j++)
 		{
 			var landmark = "";
+			var rand = 0;
+			var index = 0;
 			if(Math.random() <= 0.25)
 			{
-				landmark = items[Math.floor(Math.random()*items.length)];
+				rand = Math.floor(Math.random()*items.length);
+				console.log(rand);
+				landmark = items[rand];
+				console.log(items[rand]);
+				index = items.indexOf(landmark);
+				items.splice(index,1);
+				console.log(items);
 				var landmarkImg = emojione.emojioneList[landmark];
 				if(landmarkImg.length > 1)
 					landmarkImg = landmarkImg[landmarkImg.length - 1];
-				console.log("Using emoji "+landmark+" - "+landmarkImg);
-				console.log(landmarkImg.toString().toUpperCase());
+				//console.log("Using emoji "+landmark+" - "+landmarkImg);
+				//console.log(landmarkImg.toString().toUpperCase());
 				landmarkImg = landmarkImg.toString().toUpperCase();
 				grid += "<td style=\"background-image: url("+emojione.imagePathSVG+landmarkImg+".svg)\"></td>";
 			}
@@ -70,12 +79,18 @@ function startHandler(size){
 		//if all but one have been checked, need to make sure that
 		//the last one wins
 		console.log(size*size*0.75);
+		var winImg = ":birthday:";
+		winImg = emojione.emojioneList[winImg];
+		if(winImg.length > 1)
+			winImg = wimImg[winImg.length - 1];
+		winImg = winImg.toString().toUpperCase();
 		if(clicked >= Math.floor(size * size * 0.75))
 		{
 			if(Math.random() > 0.7 || clicked == (size*size)-1)
 			{
 				//do something to win
-				$(this).css('background-image', 'url(http://img3.wikia.nocookie.net/__cb20130119235936/cityville/images/9/94/Viral_doublerainbow_pot_of_gold_64x64.png)');
+				//$(this).css('background-image', 'url(http://img3.wikia.nocookie.net/__cb20130119235936/cityville/images/9/94/Viral_doublerainbow_pot_of_gold_64x64.png)');
+				$(this).html("<img src=\""+emojione.imagePathSVG+winImg+".svg\"></img>");
 				alert('nice one');
 				//this is a hack to get around things
 				$(this).attr('checked','1');
